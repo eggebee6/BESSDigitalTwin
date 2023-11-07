657f2c9cd53b3c9709192dfc89b5f48081260c7c
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2023-11-07 14:28:22 -0600

    Update model demo script

--- utility/demo_model.m
+++ utility/demo_model.m
@@ -1,7 +1,7 @@
-function demo_model(model, data, predict_full_sequence)
+function demo_model(model, error_vectors, predict_full_sequence)
   arguments
     model = [];
-    data = [];
+    error_vectors = [];
     predict_full_sequence = false;
   end
   gp = global_params();
@@ -9,21 +9,21 @@ function demo_model(model, data, predict_full_sequence)
   dim_C = 1;
   dim_B = 2;
   dim_T = 3;
-  size_C = size(data, dim_C);
-  size_T = size(data, dim_T);
+  size_C = size(error_vectors, dim_C);
+  size_T = size(error_vectors, dim_T);
 
   % Only use first sample from batch
-  data = data(:, 1, :);
+  error_vectors = error_vectors(:, 1, :);
   size_B = 1;
 
   % Scale data
-  data(1:3) = data(1:3) ./ gp.iLf_err_scale;
-  data(4:6) = data(4:6) ./ gp.vCf_err_scale;
-  data(7:9) = data(7:9) ./ gp.iLo_err_scale;
+  error_vectors(1:3, :) = error_vectors(1:3, :) ./ gp.iLf_err_scale;
+  error_vectors(4:6, :) = error_vectors(4:6, :) ./ gp.vCf_err_scale;
+  error_vectors(7:9, :) = error_vectors(7:9, :) ./ gp.iLo_err_scale;
 
   if (predict_full_sequence)
     % Pass entire sequence through model
-    encoder_output = predict(model.encoder, data);
+    encoder_output = predict(model.encoder, error_vectors);
     latent_sample = predict(model.latent_sampler, encoder_output);
     decoder_output = predict(model.decoder, latent_sample);
 
@@ -41,7 +41,7 @@ function demo_model(model, data, predict_full_sequence)
       end_index = start_index + gp.min_sequence_len - 1;
   
       % Forward data through model
-      encoder_output = predict(model.encoder, data(:, :, start_index:end_index));
+      encoder_output = predict(model.encoder, error_vectors(:, :, start_index:end_index));
       latent_sample = predict(model.latent_sampler, encoder_output);
       decoder_output = predict(model.decoder, latent_sample);
   
@@ -53,39 +53,43 @@ function demo_model(model, data, predict_full_sequence)
   end
 
   % Reshape for convenience
-  data = reshape(data, [size_C size_T]);
+  error_vectors = reshape(error_vectors, [size_C size_T]);
   recon_data = reshape(recon_data, [size(recon_data, 1) size(recon_data, 3)]);
 
   % Truncate data ranges to shorter length
-  max_len = min([size(data, 2), size(recon_data, 2)]);
-  data = data(:, 1:max_len);
+  max_len = min([size(error_vectors, 2), size(recon_data, 2)]);
+  error_vectors = error_vectors(:, 1:max_len);
   recon_data = recon_data(:, 1:max_len);
 
-  % Rescale output
-  recon_data(1:3) = recon_data(1:3) .* gp.iLf_err_scale;
-  recon_data(4:6) = recon_data(4:6) .* gp.vCf_err_scale;
-  recon_data(7:9) = recon_data(7:9) .* gp.iLo_err_scale;
+  % Rescale data
+  recon_data(1:3, :) = recon_data(1:3, :) .* gp.iLf_err_scale;
+  recon_data(4:6, :) = recon_data(4:6, :) .* gp.vCf_err_scale;
+  recon_data(7:9, :) = recon_data(7:9, :) .* gp.iLo_err_scale;
+
+  error_vectors(1:3, :) = error_vectors(1:3, :) .* gp.iLf_err_scale;
+  error_vectors(4:6, :) = error_vectors(4:6, :) .* gp.vCf_err_scale;
+  error_vectors(7:9, :) = error_vectors(7:9, :) .* gp.iLo_err_scale;
 
   % Set plot parameters
   x_range = (1:max_len) ./ gp.Fs;
   start_skip = 2;
 
   y_lim_ilf = max([...
-    max(data(1:3, start_skip:end), [], 'all'), ...
+    max(error_vectors(1:3, start_skip:end), [], 'all'), ...
     max(recon_data(1:3, start_skip:end), [], 'all'), ...
     10
   ]);
   y_lim_ilf = [0 extractdata(y_lim_ilf)];
 
   y_lim_vcf = max([...
-    max(data(4:6, start_skip:end), [], 'all'), ...
+    max(error_vectors(4:6, start_skip:end), [], 'all'), ...
     max(recon_data(4:6, start_skip:end), [], 'all'), ...
     4
   ]);
   y_lim_vcf = [0 extractdata(y_lim_vcf)];
 
   y_lim_ilo = max([...
-    max(data(7:9, start_skip:end), [], 'all'), ...
+    max(error_vectors(7:9, start_skip:end), [], 'all'), ...
     max(recon_data(7:9, start_skip:end), [], 'all'), ...
     4
   ]);
@@ -99,9 +103,9 @@ function demo_model(model, data, predict_full_sequence)
   hold on;
   data_range = 1:3;
   plot(...
-    x_range, data(data_range(1), :)', 'r-', ...
-    x_range, data(data_range(2), :)', 'g-', ...
-    x_range, data(data_range(3), :)', 'b-');
+    x_range, error_vectors(data_range(1), :)', 'r-', ...
+    x_range, error_vectors(data_range(2), :)', 'g-', ...
+    x_range, error_vectors(data_range(3), :)', 'b-');
   plot(...
     x_range, recon_data(data_range(1), :)', 'r.', ...
     x_range, recon_data(data_range(2), :)', 'g.', ...
@@ -117,9 +121,9 @@ function demo_model(model, data, predict_full_sequence)
   hold on;
   data_range = 4:6;
   plot(...
-    x_range, data(data_range(1), :)', 'r-', ...
-    x_range, data(data_range(2), :)', 'g-', ...
-    x_range, data(data_range(3), :)', 'b-');
+    x_range, error_vectors(data_range(1), :)', 'r-', ...
+    x_range, error_vectors(data_range(2), :)', 'g-', ...
+    x_range, error_vectors(data_range(3), :)', 'b-');
   plot(...
     x_range, recon_data(data_range(1), :)', 'r.', ...
     x_range, recon_data(data_range(2), :)', 'g.', ...
@@ -135,9 +139,9 @@ function demo_model(model, data, predict_full_sequence)
   hold on;
   data_range = 7:9;
   plot(...
-    x_range, data(data_range(1), :)', 'r-', ...
-    x_range, data(data_range(2), :)', 'g-', ...
-    x_range, data(data_range(3), :)', 'b-');
+    x_range, error_vectors(data_range(1), :)', 'r-', ...
+    x_range, error_vectors(data_range(2), :)', 'g-', ...
+    x_range, error_vectors(data_range(3), :)', 'b-');
   plot(...
     x_range, recon_data(data_range(1), :)', 'r.', ...
     x_range, recon_data(data_range(2), :)', 'g.', ...
