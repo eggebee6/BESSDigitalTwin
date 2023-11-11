2f54966429068c72bc6983a66e641bb253355173
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2023-11-10 18:46:58 -0600

    Add correct action labels

--- utility/scenario_action.m
+++ utility/scenario_action.m
@@ -3,23 +3,28 @@ function [action] = scenario_action(scenario_name)
   str = string(split(scenario_name, ' '));
   
   % Get action based on scenario
-  % TODO: Update these values appropriately
   if ( (length(str) >= 4) && (str(1) == "Fault") && (str(3) == "Location") )
     % "Fault x Location y"
-    action = sprintf('Fault location %d', str2num(str(4)));
+    location = str2double(str(4));
+    if (location == 2)
+      action = sprintf('Grid forming');
+    elseif (location == 3)
+      action = sprintf('Disconnect');
+    else
+      action = sprintf('No action');
+    end
 
   elseif ( (length(str) >= 3) && (join(str(1:3), ' ') == "Prime mover loss") )
     % "Prime mover loss"
-    action = sprintf('Prime mover loss');
+    action = sprintf('Grid forming');
 
   elseif ( (length(str) >= 3) && (join(str(1:2), ' ') == "Load step") )
     % "Load step x"
-    action = sprintf('Load step %d', str2num(str(3)));
-    %action = sprintf('Load step');
+    action = sprintf('No action');
 
   elseif ( (length(str) >= 2) && (join(str(1:2), ' ') == "No events") )
     % "No events"
-    action = sprintf('No events');
+    action = sprintf('No action');
 
   else
     % No action perscribed for scenario
