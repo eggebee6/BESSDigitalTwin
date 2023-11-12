1495c63387d5c9f7e43cd307470b3cbd38c3f991
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2023-11-12 10:37:19 -0600

    Update plot example

--- demo_plot.m
+++ demo_plot.m
@@ -25,7 +25,7 @@
 %demo_scenario_filename = 'D:/data/training/Fault 11 Location 3/scenario_25-23.mat';
 
 % A-B faults at each location
-%demo_scenario_filename = 'D:/data/training/Fault 1 Location 2/scenario_200-2.mat';
+%demo_scenario_filename = 'D:/data/training/Fault 1 Location 2/scenario_25-2.mat';
 %demo_scenario_filename = 'D:/data/training/Fault 1 Location 3/scenario_25-13.mat';
 %demo_scenario_filename = 'D:/data/training/Fault 1 Location 4/scenario_25-125.mat';
 %demo_scenario_filename = 'D:/data/training/Fault 1 Location 5/scenario_25-88.mat';
@@ -45,11 +45,20 @@
 
 % Prime mover, no events
 %demo_scenario_filename = 'D:/data/training/Prime mover loss/scenario_25-27.mat';
-demo_scenario_filename = 'D:/data/training/No events/scenario_25-1.mat';
-
-dt_info = DTInfo.read_dt_info(demo_scenario_filename);
+%demo_scenario_filename = 'D:/data/training/No events/scenario_25-1.mat';
 
 num_cycles = 5;
 
-figure('WindowState', 'maximized');
-fig_full = demo_model(model, dt_info, num_cycles);
+for demo_scenario_filename = [...
+  "D:/data/training/Fault 1 Location 2/scenario_50-2.mat", ...
+  "D:/data/training/Fault 1 Location 3/scenario_50-13.mat", ...
+  "D:/data/training/Fault 1 Location 4/scenario_50-125.mat", ...
+  "D:/data/training/Fault 1 Location 5/scenario_50-88.mat", ...
+  "D:/data/training/Fault 1 Location 6/scenario_50-51.mat", ...
+]
+  
+  dt_info = DTInfo.read_dt_info(demo_scenario_filename);
+  
+  figure('WindowState', 'maximized');
+  fig_full = demo_model(model, dt_info, num_cycles);
+end
